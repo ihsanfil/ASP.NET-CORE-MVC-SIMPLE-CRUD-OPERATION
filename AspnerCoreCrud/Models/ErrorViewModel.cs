8ecce9758922803b203b55147885dcad2844762f
ihsan <42092819+ihsanfil@users.noreply.github.com>
2021-01-26 16:30:36 +0300

    Add project files.

---
+++ AspnerCoreCrud/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspnerCoreCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
